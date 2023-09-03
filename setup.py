0f230291a98dc1df7ca6a74e08573ac48a70fe06
Chan Koon Qi <87072485+CookieDude96@users.noreply.github.com>
2023-09-04 03:14:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from version import __version__ as version
 
 
 setup(name='bq_helper',
+      packages=find_packages(include=['bq_helper', 'test_helper']),
       version=version,
       description='Helper class to simplify common read-only BigQuery tasks.',
       author='Sohier Dane',
       url='https://github.com/SohierDane/BigQuery_Helper',
       license='Apache 2.0',
       install_requires=['pandas', 'google-cloud-bigquery'],
-      classifiers=['Programming Language :: Python :: 3'],
-      packages=find_packages(include=['bq_helper', 'test_helper']),
+      classifiers=['Programming Language :: Python :: 3'],  
       )
